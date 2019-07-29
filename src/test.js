ed6fb799641f3dc84fb22134f52078d88cd97fc2
Andrew Jessen-Tyler <andrewtyl@gmail.com>
2019-07-29 11:24:06 -0500

    react testing

---
+++ src/test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function MyReactTest() {
+  return (
+    <div className="test">
+      <header className="test-header">
+        <p>
+            Testing React
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default MyReactTest;
